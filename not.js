82d3d05a53c6e9dde736164571250ecaf52b37ca
= <=>
2023-09-05 14:45:21 +0530

    script modified

--- not.js
+++ not.js
@@ -1,32 +1,32 @@
- // Add an event listener to the "Add" button
- document.getElementById('AddButton').addEventListener('click', function () {
-    // Get input values
+ 
+ document.getElementById('AddButton').addEventListener('click', ()=> {
+    
     const site = document.getElementById('Site').value;
     const username = document.getElementById('UserName').value;
     const password = document.getElementById('Passwords').value;
 
-    // Create an object to represent the entry
+   
     const entry = { site, username, password };
 
-    // Check if local storage already has data
+    
     let entries = JSON.parse(localStorage.getItem('passwordEntries')) || [];
 
-    // Add the new entry to the array
+   
     entries.push(entry);
 
-    // Store the updated array in local storage
+    
     localStorage.setItem('passwordEntries', JSON.stringify(entries));
 
-    // Clear input fields
+    
     document.getElementById('Site').value = '';
     document.getElementById('UserName').value = '';
     document.getElementById('Passwords').value = '';
 
-    // Update the table
+    
     updateTable();
 });
 
-// Function to update the table from local storage
+// updating the table from local storage
 function updateTable() {
     const tableBody = document.getElementById('tableBody');
     tableBody.innerHTML = ''; // Clear the table body
@@ -45,12 +45,12 @@ function updateTable() {
     }
 }
 
-// Load table data when the page loads
+
 updateTable();
 
 
 
-// for deleting the entries
+
 
 document.getElementById('del').addEventListener('click',()=>{
     localStorage.clear()
