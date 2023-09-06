515b685cc00c54ac8af3c19616db2c0267b904e3
= <=>
2023-09-06 19:51:19 +0530

    js updated

--- not.js
+++ not.js
@@ -1,37 +1,37 @@
  
  document.getElementById('AddButton').addEventListener('click', ()=> {
     
-    const site = document.getElementById('Site').value;
-    const username = document.getElementById('UserName').value;
-    const password = document.getElementById('Passwords').value;
+    const site = document.getElementById('Site').value
+    const username = document.getElementById('UserName').value
+    const password = document.getElementById('Passwords').value
 
    
-    const entry = { site, username, password };
+    const entry = { site, username, password }
 
     
-    let entries = JSON.parse(localStorage.getItem('passwordEntries')) || [];
+    let entries = JSON.parse(localStorage.getItem('passwordEntries')) || []
 
    
-    entries.push(entry);
+    entries.push(entry)
 
     
-    localStorage.setItem('passwordEntries', JSON.stringify(entries));
+    localStorage.setItem('passwordEntries', JSON.stringify(entries))
 
     
-    document.getElementById('Site').value = '';
-    document.getElementById('UserName').value = '';
-    document.getElementById('Passwords').value = '';
+    document.getElementById('Site').value = ''
+    document.getElementById('UserName').value = ''
+    document.getElementById('Passwords').value = ''
 
     
-    updateTable();
+    updateTable()
 });
 
 // updating the table from local storage
 function updateTable() {
-    const tableBody = document.getElementById('tableBody');
-    tableBody.innerHTML = ''; // Clear the table body
+    const tableBody = document.getElementById('tableBody')
+    tableBody.innerHTML = '';
 
-    const entries = JSON.parse(localStorage.getItem('passwordEntries')) || [];
+    const entries = JSON.parse(localStorage.getItem('passwordEntries')) || []
 
     for (const entry of entries) {
         const row = tableBody.insertRow();
@@ -54,5 +54,23 @@ updateTable();
 
 document.getElementById('del').addEventListener('click',()=>{
     localStorage.clear()
-    location.reload()
+    location.reload()       //have to change this later on
 })
+
+
+
+// copy button
+function myFunction() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  }
